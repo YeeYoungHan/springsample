3f9fad0ba8b4fae825f2a60688ab48278c7bb9aa
Yee Young Han <websearch@naver.com>
2018-06-21 08:50:11 +0900

    utf8 로 정상적으로 저장될 수 있도록 수정함

--- SpringNoticeBoard/sql/CreateTable.sql
+++ SpringNoticeBoard/sql/CreateTable.sql
@@ -5,8 +5,8 @@ create DATABASE spring default CHARACTER SET = UTF8;
 DROP TABLE IF EXISTS `spring`.`noticeboard`;
 CREATE TABLE  `spring`.`noticeboard` (
   `nbId` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `nbSubject` varchar(255) CHARACTER SET latin1 NOT NULL,
-  `nbContent` text CHARACTER SET latin1 NOT NULL,
+  `nbSubject` varchar(255) NOT NULL,
+  `nbContent` text NOT NULL,
   `nbReadCount` int(10) unsigned NOT NULL DEFAULT '0',
   `nbInsertDate` datetime NOT NULL,
   PRIMARY KEY (`nbId`)
