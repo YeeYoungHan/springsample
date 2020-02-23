9a6211c56d560c642dcdbcb16ff6dd59b89b41eb
Yee Young Han <websearch@naver.com>
2020-02-23 16:18:15 +0900

    마이바티스 SELECT ONE 기능 테스트

---
+++ TestMyBatis/src/com/test/TestMyBatisXml.java
@@ -0,0 +1,52 @@
+package com.test;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class TestMyBatisXml
+{
+	String m_strConfigPath = "com/test/Config.xml";
+	SqlSessionFactory m_clsFactory = null;
+	
+	SqlSessionFactory CreateSqlSessionFactory()
+	{
+		if( m_clsFactory == null )
+		{
+			try
+			{
+				InputStream clsInput = Resources.getResourceAsStream( m_strConfigPath );
+				m_clsFactory = new SqlSessionFactoryBuilder().build( clsInput );
+			}
+			catch( Exception e )
+			{
+				System.out.println( "ERROR(" + e.toString( ) + ")" );
+				return null;
+			}
+		}
+		
+		return m_clsFactory;
+	}
+	
+	String SelectSubject( int iId )
+	{
+		SqlSessionFactory clsFactory = CreateSqlSessionFactory();
+		if( clsFactory == null ) return null;
+		
+		SqlSession clsSession = clsFactory.openSession( );
+		
+		return clsSession.selectOne( "com.test.TestMyBatis.SelectSubject", iId );
+	}
+	
+	public static void main( String [] args )
+	{
+		TestMyBatisXml clsTest = new TestMyBatisXml();
+		
+		String strName = clsTest.SelectSubject( 3 );
+		
+		System.out.println( "SelectName(3) => (" + strName + ")" );
+	}
+}
