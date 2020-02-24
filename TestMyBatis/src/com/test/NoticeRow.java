c4b275aab888d44bfdb8772ffe18d9d28c763c15
Yee Young Han <websearch@naver.com>
2020-02-24 10:16:27 +0900

    하나의 ROW 에 포함된 모든 컬럼들을 가져오는 예제 추가함

---
+++ TestMyBatis/src/com/test/NoticeRow.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.util.Date;
+
+public class NoticeRow
+{
+	String m_strSubject;
+	String m_strContent;
+	int m_iReadCount;
+	Date m_clsInsertDate;
+	int m_iCommentCount;
+	
+	public String toString()
+	{
+		return "subject(" + m_strSubject + ") content(" + m_strContent + ") readCount(" + m_iReadCount + ") insertDate(" + m_clsInsertDate + ") commentCount(" + m_iCommentCount + ")";
+	}
+}
--- TestMyBatis/src/com/test/TestMyBatisXml.java
+++ TestMyBatis/src/com/test/TestMyBatisXml.java
@@ -7,11 +7,20 @@ import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
+/** XML 기반 마이바티스 테스트 클래스
+ * 
+ * @author 이영한 ( http://blog.naver.com/websearch )
+ *
+ */
 public class TestMyBatisXml
 {
 	String m_strConfigPath = "com/test/Config.xml";
 	SqlSessionFactory m_clsFactory = null;
 	
+	/** SqlSessionFactory 를 리턴한다.
+	 * 
+	 * @return SqlSessionFactory 를 리턴한다.
+	 */
 	SqlSessionFactory CreateSqlSessionFactory()
 	{
 		if( m_clsFactory == null )
@@ -31,25 +40,48 @@ public class TestMyBatisXml
 		return m_clsFactory;
 	}
 	
+	/** 하나의 ROW 에 포함된 하나의 컬럼을 가져오는 예제
+	 * 
+	 * @param iId noticeboard 테이블의 nbId 컬럼값
+	 * @return noticeboard 테이블의 nbId 컬럼과 일치하는 nbSubject 컬럼값을 리턴한다.
+	 */
 	String SelectSubject( int iId )
 	{
 		SqlSessionFactory clsFactory = CreateSqlSessionFactory();
 		if( clsFactory == null ) return null;
 		
-		SqlSession clsSession = clsFactory.openSession( );
+		try( SqlSession clsSession = clsFactory.openSession( ) )
+		{
+			return clsSession.selectOne( "com.test.TestMyBatis.SelectSubject", iId );
+		}
+	}
+	
+	/** 하나의 ROW 에 포함된 모든 컬럼들을 가져오는 예제
+	 * 
+	 * @param iId noticeboard 테이블의 nbId 컬럼값
+	 * @return noticeboard 테이블의 nbId 컬럼과 일치하는 모든 컬럼을 저장한 NoticeRow 객체를 리턴한다.
+	 */
+	NoticeRow SelectRow( int iId )
+	{
+		SqlSessionFactory clsFactory = CreateSqlSessionFactory();
+		if( clsFactory == null ) return null;
 		
-		String strSubject = clsSession.selectOne( "com.test.TestMyBatis.SelectSubject", iId );
-		clsSession.close();
-		
-		return strSubject;
+		try( SqlSession clsSession = clsFactory.openSession( ) )
+		{
+			return clsSession.selectOne( "com.test.TestMyBatis.SelectRow", iId );
+		}
 	}
 	
 	public static void main( String [] args )
 	{
 		TestMyBatisXml clsTest = new TestMyBatisXml();
-		
+
+		/*
 		String strName = clsTest.SelectSubject( 3 );
-		
 		System.out.println( "SelectName(3) => (" + strName + ")" );
+		*/
+		
+		NoticeRow clsRow = clsTest.SelectRow( 3 );
+		System.out.println( clsRow.toString() );
 	}
 }
