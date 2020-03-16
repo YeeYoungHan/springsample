0304e5f50d7afe8aae7eff5a8e26dbb749d205fb
Yee Young Han <websearch@naver.com>
2020-03-16 23:07:11 +0900

    annotation 기반으로 SQL DELETE 명령을 실행하는 예제를 추가함

---
+++ TestSpring/src/main/java/com/test/TestJdbcTemplateAnnotation.java
@@ -0,0 +1,58 @@
+package com.test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+@Configuration
+public class TestJdbcTemplateAnnotation
+{
+	@Bean
+	public SimpleDriverDataSource dataSource()
+	{
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass( org.mariadb.jdbc.Driver.class );
+		dataSource.setUrl( "jdbc:mysql://localhost:3306/spring" );
+		dataSource.setUsername( "spring" );
+		dataSource.setPassword( "spring" );
+		
+		return dataSource;
+	}
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate()
+	{
+		return new JdbcTemplate( dataSource() );
+	}
+	
+	@Bean
+	public TestJdbcTemplate2 testJdbcTemplate()
+	{
+		return new TestJdbcTemplate2();
+	}
+	
+	public static class TestJdbcTemplate2
+	{
+		@Autowired
+		JdbcTemplate jdbcTemplate;
+		
+		public void Delete( int iId )
+		{
+			jdbcTemplate.update( "DELETE FROM noticeboard WHERE nbId = ?", iId );
+		}
+	}
+	
+	public static void main( String [] args )
+	{
+		ApplicationContext context = new AnnotationConfigApplicationContext( TestJdbcTemplateAnnotation.class );
+		
+		TestJdbcTemplate2 clsTest = context.getBean( "testJdbcTemplate", TestJdbcTemplate2.class );
+		
+		clsTest.Delete( 5 );
+	}
+}
