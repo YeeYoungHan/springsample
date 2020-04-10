86f002c5c1903e6464a4e0de83ba41b872344374
Yee Young Han <websearch@naver.com>
2020-04-10 11:42:49 +0900

    AOP 를 이용한 Dynamic Proxy 생성

--- TestSpring/src/main/java/com/test/TestAop.java
+++ TestSpring/src/main/java/com/test/TestAop.java
@@ -4,15 +4,15 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
-/** AOP 를 이용한 DefaultAdvisorAutoProxyCreator 테스트
+/** AOP 를 이용한 Dynamic Proxy 생성 테스트
  * 
  * @author 이영한 ( http://blog.naver.com/websearch )
  */
-public class TestDefaultAdvisorAutoProxyCreatorAop
+public class TestAop
 {
 	public static void main( String [] args )
 	{
-		ApplicationContext context = new GenericXmlApplicationContext("com/test/advisorautoproxyaop.xml");
+		ApplicationContext context = new GenericXmlApplicationContext("com/test/aop.xml");
 		
 		TestJdbcTemplateInterface clsTest = context.getBean( "testJdbcTemplate", TestJdbcTemplateInterface.class );
 		
