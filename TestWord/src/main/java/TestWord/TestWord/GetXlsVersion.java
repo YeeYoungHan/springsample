9f480cff2bbf2d38e3857bc74e9633945f7f14b0
Yee Young Han <websearch@naver.com>
2021-03-31 17:11:18 +0900

    엑셀 파일을 프로그램 실행 인자로 받는다. 엑셀 버전을 문자열로 출력한다.

--- TestWord/src/main/java/TestWord/TestWord/GetXlsVersion.java
+++ TestWord/src/main/java/TestWord/TestWord/GetXlsVersion.java
@@ -8,14 +8,42 @@ public class GetXlsVersion
 {
 	public static void main( String [] args ) throws Exception
 	{
-		HSSFWorkbook workbook = new HSSFWorkbook( new FileInputStream( "c:/temp/1.xls" ) );
+		if( args.length != 1 )
+		{
+			System.out.println( "[Usage] java GetXlsVersion {excel file path}" );
+			return;
+		}
+		
+		HSSFWorkbook workbook = new HSSFWorkbook( new FileInputStream( args[0] ) );
 		
 		int iVersion = workbook.getDocumentSummaryInformation().getApplicationVersion();
 		
+		workbook.close();
+		
 		// 엑셀 프로그램 버전을 출력한다.
 		// 버전 정보는 https://blog.naver.com/websearch/222268534563 에 기술되어 있음
-		System.out.println( ((iVersion&0xFF0000) >> 16 ) );
+		iVersion = ((iVersion&0xFF0000) >> 16 );
+		System.out.println( iVersion );
 		
-		workbook.close();
+		String strVersion = "";
+		
+		switch( iVersion )
+		{
+		case 2: strVersion = "Excel 2.0"; break;
+		case 3: strVersion = "Excel 3.0"; break;
+		case 4: strVersion = "Excel 4.0"; break;
+		case 5: strVersion = "Excel 5.0"; break;
+		case 7: strVersion = "Excel 95"; break;
+		case 8: strVersion = "Excel 97"; break;
+		case 9: strVersion = "Excel 2000"; break;
+		case 10: strVersion = "Excel 2002"; break;
+		case 11: strVersion = "Excel 2003"; break;
+		case 12: strVersion = "Excel 2007"; break;
+		case 14: strVersion = "Excel 2010"; break;
+		case 15: strVersion = "Excel 2013"; break;
+		case 16: strVersion = "Excel 2016"; break;
+		}
+		
+		System.out.println( strVersion );
 	}
 }
