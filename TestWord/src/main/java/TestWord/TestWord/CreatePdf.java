e1971926978bdc89eba063aae787eed0be955d49
Yee Young Han <websearch@naver.com>
2021-10-18 17:20:13 +0900

    pdf 에 한글 저장 기능 추가함

--- TestWord/src/main/java/TestWord/TestWord/CreatePdf.java
+++ TestWord/src/main/java/TestWord/TestWord/CreatePdf.java
@@ -3,6 +3,9 @@ package TestWord.TestWord;
 import java.io.FileOutputStream;
 
 import com.itextpdf.text.Document;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.BaseFont;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
@@ -10,6 +13,10 @@ public class CreatePdf
 {
 	public static void main( String [] args ) throws Exception
 	{
+		String strWorkFolder = System.getProperty("user.dir");
+		BaseFont clsBaseFont = BaseFont.createFont( strWorkFolder + "/NanumBarunGothic-Regular.ttf" , BaseFont.IDENTITY_H, BaseFont.EMBEDDED );
+		Font clsFont = new Font( clsBaseFont, 10 );
+		
 		Document clsDoc = new Document();
     PdfWriter.getInstance( clsDoc, new FileOutputStream("c:/temp/1.pdf") );
       
@@ -20,7 +27,8 @@ public class CreatePdf
      {
     	 for( int iCol = 0; iCol < 4; ++iCol )
     	 {
-    		 table.addCell( "row_" + iRow + ", col_" + iCol );
+    		 //table.addCell( "row_" + iRow + ", col_" + iCol );
+    		 table.addCell( new Phrase( "행" + iRow + ", 열" + iCol, clsFont ) );
     	 }
      }
 
