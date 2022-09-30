7bf6916176278a5ad5d72b6d15f5c7d2a68dd91d
Yee Young Han <websearch@naver.com>
2022-09-30 12:37:39 +0900

    xls 행수 제한으로 Exception 이 발생하지 않도록 수정함

--- TestWord/src/main/java/TestWord/TestWord/CreateXls.java
+++ TestWord/src/main/java/TestWord/TestWord/CreateXls.java
@@ -1,5 +1,7 @@
 package TestWord.TestWord;
 
+import java.io.File;
+
 import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
@@ -11,7 +13,27 @@ public class CreateXls
 	{
 		HSSFWorkbook clsWorkBook = new HSSFWorkbook();
 		HSSFSheet clsSheet = clsWorkBook.createSheet( );
+		int iRowIndex = 0;
 		
+		for( int i = 0; i < 1050000; ++i )
+		{
+			HSSFRow clsRow = clsSheet.createRow( iRowIndex );
+			
+			HSSFCell clsCell = clsRow.createCell( 0 );
+			clsCell.setCellValue( Integer.toString( iRowIndex ) );
+			
+			++iRowIndex;
+			
+			if( iRowIndex == 60000 )
+			{
+				clsSheet = clsWorkBook.createSheet( );
+				iRowIndex = 0;
+			}
+		}
+		
+		clsWorkBook.write( new File("c:/temp/1.xls") );
+		
+		/* 아래와 같이 실행하면 Invalid row number (65536) outside allowable range (0..65535) Exception 이 발생한다.
 		for( int iRowIndex = 0; iRowIndex < 1050000; ++iRowIndex )
 		{
 			HSSFRow clsRow = clsSheet.createRow( iRowIndex );
@@ -19,5 +41,6 @@ public class CreateXls
 			HSSFCell clsCell = clsRow.createCell( 0 );
 			clsCell.setCellValue( Integer.toString( iRowIndex ) );
 		}
+		*/
 	}
 }
