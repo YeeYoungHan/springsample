b313608b5a19887bc7a0898f905b8f621c132995
Yee Young Han <websearch@naver.com>
2023-07-26 16:35:08 +0900

    다양한 객체에 대한 ResponseBody 테스트 추가함

--- TestSpringMvc/src/main/java/com/test/mvc/JsonStrategyController.java
+++ TestSpringMvc/src/main/java/com/test/mvc/JsonStrategyController.java
@@ -18,6 +18,9 @@
 
 package com.test.mvc;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,6 +72,9 @@ public class JsonStrategyController
 		return strHtml;
 	}
 	
+	/* 아래와 같이 응답한다.
+	 * {"strName":"name@1","iValue":1234,"arrInt":null,"arrString":null,"lstInt":null,"lstString":null}
+	 */
 	@ResponseBody
 	@RequestMapping(value = "/get_user4", method = RequestMethod.GET)
 	public User4 GetUser4( )
@@ -81,11 +87,163 @@ public class JsonStrategyController
 		return clsUser;
 	}
 	
+	/* 아래와 같이 응답한다.
+	 * {"strName":"name@1","iValue":1234,"arrInt":[0,0,0],"arrString":[null,null,null],"lstInt":[],"lstString":[]}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user41", method = RequestMethod.GET)
+	public User4 GetUser41( )
+	{
+		User4 clsUser = new User4();
+		
+		clsUser.m_strName = "name@1";
+		clsUser.m_iValue = 1234;
+		clsUser.m_arrInt = new int[3];
+		clsUser.m_arrString = new String[3];
+		clsUser.m_lstInt = new ArrayList<Integer>();
+		clsUser.m_lstString = new ArrayList<String>();
+		
+		return clsUser;
+	}
+	
+	/* 아래와 같이 응답한다.
+	 * {"strName":"name@1","iValue":1234,"arrInt":[2,0,0],"arrString":["first",null,null],"lstInt":[3],"lstString":["first-list"]}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user42", method = RequestMethod.GET)
+	public User4 GetUser42( )
+	{
+		User4 clsUser = new User4();
+		
+		clsUser.m_strName = "name@1";
+		clsUser.m_iValue = 1234;
+		clsUser.m_arrInt = new int[3];
+		clsUser.m_arrString = new String[3];
+		clsUser.m_lstInt = new ArrayList<Integer>();
+		clsUser.m_lstString = new ArrayList<String>();
+		
+		clsUser.m_arrInt[0] = 2;
+		clsUser.m_arrString[0] = "first";
+		clsUser.m_lstInt.add( 3 );
+		clsUser.m_lstString.add( "first-list" );
+		
+		return clsUser;
+	}
+
+	/* 아래와 같이 응답한다.
+	 * {"mapInt":null,"mapString":null}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user5", method = RequestMethod.GET)
+	public User5 GetUser5( )
+	{
+		User5 clsUser = new User5();
+		
+		return clsUser;
+	}
+	
+	/* 아래와 같이 응답한다.
+	 * {"mapInt":{},"mapString":{}}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user51", method = RequestMethod.GET)
+	public User5 GetUser51( )
+	{
+		User5 clsUser = new User5();
+		
+		clsUser.m_mapInt = new HashMap<Integer,Integer>();
+		clsUser.m_mapString = new HashMap<String,String>();
+		
+		return clsUser;
+	}
+	
+	/* 아래와 같이 응답한다.
+	 * {"mapInt":{"1":2},"mapString":{"key":"value"}}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user52", method = RequestMethod.GET)
+	public User5 GetUser52( )
+	{
+		User5 clsUser = new User5();
+		
+		clsUser.m_mapInt = new HashMap<Integer,Integer>();
+		clsUser.m_mapString = new HashMap<String,String>();
+		
+		clsUser.m_mapInt.put( 1, 2 );
+		clsUser.m_mapString.put( "key", "value" );
+		
+		return clsUser;
+	}
+	
+	/* 아래와 같이 응답한다.
+	 * {"arrUser":null}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user6", method = RequestMethod.GET)
+	public User6 GetUser6( )
+	{
+		User6 clsUser = new User6();
+		
+		return clsUser;
+	}
+	
+	/* 아래와 같이 응답한다.
+	 * {"arrUser":[]}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user61", method = RequestMethod.GET)
+	public User6 GetUser61( )
+	{
+		User6 clsUser = new User6();
+		
+		clsUser.m_arrUser = new ArrayList<User4>();
+		
+		return clsUser;
+	}
+	
+	/* 아래와 같이 응답한다.
+	 * {"arrUser":[{"strName":"name","iValue":1,"arrInt":null,"arrString":null,"lstInt":null,"lstString":null}]}
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/get_user62", method = RequestMethod.GET)
+	public User6 GetUser62( )
+	{
+		User6 clsUser = new User6();
+		
+		clsUser.m_arrUser = new ArrayList<User4>();
+		
+		User4 clsTemp = new User4();
+		
+		clsTemp.m_strName = "name";
+		clsTemp.m_iValue = 1;
+		
+		clsUser.m_arrUser.add( clsTemp );
+		
+		return clsUser;
+	}
+	
 	@JsonNaming(MyClassStrategy.class)
 	static class User4
 	{
 		public String m_strName;
 		public int m_iValue;
+		public int [] m_arrInt;
+		public String [] m_arrString;
+		public ArrayList<Integer> m_lstInt;
+		public ArrayList<String> m_lstString;
+	}
+	
+	@JsonNaming(MyClassStrategy.class)
+	static class User5
+	{
+		public HashMap<Integer,Integer> m_mapInt;
+		public HashMap<String,String> m_mapString;
+	}
+	
+	@JsonNaming(MyClassStrategy.class)
+	static class User6
+	{
+		public ArrayList<User4> m_arrUser;
 	}
 	
 	public static class MyClassStrategy extends PropertyNamingStrategyBase
